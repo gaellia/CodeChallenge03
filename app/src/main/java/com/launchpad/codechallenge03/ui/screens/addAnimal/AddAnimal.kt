0e3c6859d4fd2c91849bda2a7b0a714a2ddfc8ba
Kevin Vo <25157215+gaellia@users.noreply.github.com>
2021-11-16 13:37:42 -0700

    soft keyboard workaround when adding an animal

--- app/src/main/java/com/launchpad/codechallenge03/ui/screens/addAnimal/AddAnimal.kt
+++ app/src/main/java/com/launchpad/codechallenge03/ui/screens/addAnimal/AddAnimal.kt
@@ -1,8 +1,9 @@
 package com.launchpad.codechallenge03.ui.screens.addAnimal
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -10,23 +11,29 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.relocationRequester
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.launchpad.codechallenge03.models.Animal
 import com.launchpad.codechallenge03.ui.theme.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
+@ExperimentalFoundationApi
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @Composable
@@ -39,6 +46,7 @@ fun AddAnimal(
 }
 
 
+@ExperimentalFoundationApi
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @Composable
@@ -59,6 +67,7 @@ private fun AddAnimalState(
 }
 
 
+@ExperimentalFoundationApi
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @Composable
@@ -97,11 +106,14 @@ private fun AddAnimalContent(
                 .clickable(
                     enabled = viewState.name.isNotBlank()
                 ) {
-                    actioner(AddAnimalAction.InsertAnimal(
-                        name = viewState.name,
-                        type = viewState.type,
-                        size = viewState.size,
-                        description = viewState.description))
+                    actioner(
+                        AddAnimalAction.InsertAnimal(
+                            name = viewState.name,
+                            type = viewState.type,
+                            size = viewState.size,
+                            description = viewState.description
+                        )
+                    )
                     actioner(AddAnimalAction.NavigateBack)
                 },
                 imageVector = Icons.Default.Check,
@@ -110,168 +122,188 @@ private fun AddAnimalContent(
             )
         }
 
-        // FIELDS
-        val keyboardController = LocalSoftwareKeyboardController.current
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            // FIELDS
+            val keyboardController = LocalSoftwareKeyboardController.current
 
-        Text(modifier = Modifier
-            .padding(vertical = 10.dp, horizontal = 20.dp)
-            .fillMaxWidth(),
-            text = "Name",
-            style = MaterialTheme.typography.body1
-        )
-        TextField(modifier = Modifier
-            .padding(horizontal = 20.dp)
-            .fillMaxWidth(),
-            value = viewState.name,
-            singleLine = true,
-            shape = RoundedCornerShape(10.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                textColor = Color.Black
-            ),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(
-                onDone = {keyboardController?.hide()}
-            ),
-            onValueChange = { newName -> actioner(AddAnimalAction.UpdateName(newName)) }
-        )
-
-        Text(modifier = Modifier
-            .padding(vertical = 10.dp, horizontal = 20.dp)
-            .fillMaxWidth(),
-            text = "Type",
-            style = MaterialTheme.typography.body1
-        )
-        ExposedDropdownMenuBox(
-            modifier = Modifier
+            Text(modifier = Modifier
+                .padding(vertical = 10.dp, horizontal = 20.dp)
+                .fillMaxWidth(),
+                text = "Name",
+                style = MaterialTheme.typography.body1
+            )
+            TextField(modifier = Modifier
                 .padding(horizontal = 20.dp)
                 .fillMaxWidth(),
-            expanded = viewState.expandedTypeMenu,
-            onExpandedChange = {
-                actioner(AddAnimalAction.ToggleTypeMenu)
-            }
-        ) {
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                readOnly = true,
-                value = viewState.type.name,
-                onValueChange = { },
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = viewState.expandedTypeMenu
-                    )
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(
+                value = viewState.name,
+                singleLine = true,
+                shape = RoundedCornerShape(10.dp),
+                colors = TextFieldDefaults.textFieldColors(
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                     textColor = Color.Black
                 ),
-                shape = RoundedCornerShape(10.dp)
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}
+                ),
+                onValueChange = { newName -> actioner(AddAnimalAction.UpdateName(newName)) }
             )
-            ExposedDropdownMenu(
-                modifier = Modifier.fillMaxWidth(),
+
+            Text(modifier = Modifier
+                .padding(vertical = 10.dp, horizontal = 20.dp)
+                .fillMaxWidth(),
+                text = "Type",
+                style = MaterialTheme.typography.body1
+            )
+            ExposedDropdownMenuBox(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth(),
                 expanded = viewState.expandedTypeMenu,
-                onDismissRequest = {
+                onExpandedChange = {
                     actioner(AddAnimalAction.ToggleTypeMenu)
                 }
             ) {
-                enumValues<Animal.Type>().forEach { selectionOption ->
-                    DropdownMenuItem(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            actioner(AddAnimalAction.UpdateType(selectionOption))
-                            actioner(AddAnimalAction.ToggleTypeMenu)
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    readOnly = true,
+                    value = viewState.type.name,
+                    onValueChange = { },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = viewState.expandedTypeMenu
+                        )
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        textColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                ExposedDropdownMenu(
+                    modifier = Modifier.fillMaxWidth(),
+                    expanded = viewState.expandedTypeMenu,
+                    onDismissRequest = {
+                        actioner(AddAnimalAction.ToggleTypeMenu)
+                    }
+                ) {
+                    enumValues<Animal.Type>().forEach { selectionOption ->
+                        DropdownMenuItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                actioner(AddAnimalAction.UpdateType(selectionOption))
+                                actioner(AddAnimalAction.ToggleTypeMenu)
+                            }
+                        ) {
+                            Text(modifier = Modifier.fillMaxWidth(),
+                                text = selectionOption.name)
                         }
-                    ) {
-                        Text(modifier = Modifier.fillMaxWidth(),
-                            text = selectionOption.name)
                     }
                 }
             }
-        }
 
-        Text(modifier = Modifier
-            .padding(vertical = 10.dp, horizontal = 20.dp)
-            .fillMaxWidth(),
-            text = "Size",
-            style = MaterialTheme.typography.body1
-        )
-        ExposedDropdownMenuBox(
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
+            Text(modifier = Modifier
+                .padding(vertical = 10.dp, horizontal = 20.dp)
                 .fillMaxWidth(),
-            expanded = viewState.expandedSizeMenu,
-            onExpandedChange = {
-                actioner(AddAnimalAction.ToggleSizeMenu)
+                text = "Size",
+                style = MaterialTheme.typography.body1
+            )
+            ExposedDropdownMenuBox(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth(),
+                expanded = viewState.expandedSizeMenu,
+                onExpandedChange = {
+                    actioner(AddAnimalAction.ToggleSizeMenu)
+                }
+            ) {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    readOnly = true,
+                    value = viewState.size.name,
+                    onValueChange = { },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = viewState.expandedSizeMenu
+                        )
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        textColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                ExposedDropdownMenu(
+                    modifier = Modifier.fillMaxWidth(),
+                    expanded = viewState.expandedSizeMenu,
+                    onDismissRequest = {
+                        actioner(AddAnimalAction.ToggleSizeMenu)
+                    }
+                ) {
+                    enumValues<Animal.Size>().forEach { selectionOption ->
+                        DropdownMenuItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                actioner(AddAnimalAction.UpdateSize(selectionOption))
+                                actioner(AddAnimalAction.ToggleSizeMenu)
+                            }
+                        ) {
+                            Text(modifier = Modifier.fillMaxWidth(),
+                                text = selectionOption.name)
+                        }
+                    }
+                }
             }
-        ) {
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                readOnly = true,
-                value = viewState.size.name,
-                onValueChange = { },
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = viewState.expandedSizeMenu
-                    )
+
+            Text(modifier = Modifier
+                .padding(vertical = 10.dp, horizontal = 20.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f),
+                text = "Description",
+                style = MaterialTheme.typography.body1
+            )
+
+            val bringIntoViewRequester = remember { BringIntoViewRequester() }
+            val coroutineScope = rememberCoroutineScope()
+
+            TextField(modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .heightIn(min = 150.dp, Dp.Infinity)
+                .bringIntoViewRequester(bringIntoViewRequester)
+                .onFocusEvent {
+                    if (it.isFocused) coroutineScope.launch {
+                        delay(100)
+                        bringIntoViewRequester.bringIntoView()
+                    }
                 },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(
+                value = viewState.description,
+                shape = RoundedCornerShape(10.dp),
+                colors = TextFieldDefaults.textFieldColors(
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                     textColor = Color.Black
                 ),
-                shape = RoundedCornerShape(10.dp)
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}
+                ),
+                maxLines = 15,
+                onValueChange = {
+                    newDesc -> actioner(AddAnimalAction.UpdateDescription(newDesc))
+                }
             )
-            ExposedDropdownMenu(
-                modifier = Modifier.fillMaxWidth(),
-                expanded = viewState.expandedSizeMenu,
-                onDismissRequest = {
-                    actioner(AddAnimalAction.ToggleSizeMenu)
-                }
-            ) {
-                enumValues<Animal.Size>().forEach { selectionOption ->
-                    DropdownMenuItem(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            actioner(AddAnimalAction.UpdateSize(selectionOption))
-                            actioner(AddAnimalAction.ToggleSizeMenu)
-                        }
-                    ) {
-                        Text(modifier = Modifier.fillMaxWidth(),
-                            text = selectionOption.name)
-                    }
-                }
-            }
         }
 
-        Text(modifier = Modifier
-            .padding(vertical = 10.dp, horizontal = 20.dp)
-            .fillMaxWidth(),
-            text = "Description",
-            style = MaterialTheme.typography.body1
-        )
-        TextField(modifier = Modifier
-            .padding(horizontal = 20.dp)
-            .fillMaxWidth()
-            .fillMaxHeight(0.8f),
-            value = viewState.description,
-            shape = RoundedCornerShape(10.dp),
-            colors = TextFieldDefaults.textFieldColors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                textColor = Color.Black
-            ),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(
-                onDone = {keyboardController?.hide()}
-            ),
-            onValueChange = {newDesc -> actioner(AddAnimalAction.UpdateDescription(newDesc))}
-        )
+
     }
 }
 
 
+@ExperimentalFoundationApi
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @Preview (showBackground = true)
